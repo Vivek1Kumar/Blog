4a79af32d6941de09d80caaa03ad2968aa1b33aa
Vivek Yadav <vivekyadav264@gmail.com>
2018-09-19 14:31:08 -0700

    blog project pushing

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const user = require('./routes/api/user');
+const posts = require('./routes/api/posts');
+const passport = require('passport');
+//const ProfileDb = require('././models/BlogpostDB');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+mongoose.Promise = global.Promise;
+// DataBase configure
+const db = require('./config/keys').mongoURI;
+
+//Connect server to MongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log("MongoDB connected"))
+	.catch(err => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+
+
+//use Routes
+app.use('/api/user', user);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+	console.log("server running port", port);
+});
